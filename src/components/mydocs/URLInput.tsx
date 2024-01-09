e60e2e9fc0e48a0358a846812bba100ea2aae23e
Jaehoon <gkdwngo@gmail.com>
2024-01-10 00:10:06 +0900

    feat: 문서 생성 API 호출 성공

--- src/components/mydocs/URLInput.tsx
+++ src/components/mydocs/URLInput.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const Wrapper = styled.div`
   display: flex;
@@ -27,11 +28,54 @@ const StyledInput = styled.input`
   outline: none;
   text-align: center;
 `
-
 export const URLInput: React.FC = () => {
+  const [url, setUrl] = useState('')
+  const apiUrl = 'http://gtd.kro.kr:8000/api/v1/docs/create/'
+  const language = 'KOR'
+
+  // 입력값은 이 함수를 벗어나면 알 수 없으므로 state로 관리
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value)
+  }
+
+  const handleGenDoc = async () => {
+    try {
+      // API 호출
+      const response = await axios.post(`${apiUrl}`, { repository_url: url, language: language })
+      console.log(response.status)
+      console.log(response)
+      setUrl('')
+
+      //문서 생성 성공
+      if (response.status === 201) {
+        console.log('API Response: ', response.status)
+        alert(response.statusText)
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 400) {
+        console.error('API Response: ', error.response.status)
+        alert(error.response.message)
+        setUrl('')
+      }
+    }
+  }
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (url != '' && e.key === 'Enter') {
+      e.preventDefault()
+      handleGenDoc()
+    }
+  }
+
   return (
     <Wrapper>
-      <StyledInput type="text" placeholder="Please enter your GitHub repository URL here..." />
+      <StyledInput
+        type="text"
+        value={url}
+        placeholder="Please enter your GitHub repository URL here..."
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+      />
     </Wrapper>
   )
 }
