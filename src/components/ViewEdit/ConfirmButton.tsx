931ec2961b389c464922a3abf160b95dc97a3fc3
오채영 <ocy5007@naver.com>
2024-01-14 23:57:31 +0900

    design: 모달창 다크모드

--- src/components/ViewEdit/ConfirmButton.tsx
+++ src/components/ViewEdit/ConfirmButton.tsx
@@ -1,36 +1,43 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import { useDarkModeStore } from '../../store/store'
 
-const StyledConfirmButton = styled.button`
-    width: 130px;
-    height: 50px;
-    border: 1px solid transparent;
-    border-radius: 30px;
-    background-image: linear-gradient(#fff, #fff),
-    linear-gradient(#202020, #202020);
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.20);
-    background-origin: border-box;
-    background-clip: padding-box, border-box;
-    transition: linear .5s;
+const StyledConfirmButton = styled.button<{ isDarkMode: boolean }>`
+  width: 130px;
+  height: 50px;
+  border: 1px solid transparent;
+  border-radius: 30px;
+  background-image: ${(props) =>
+    props.isDarkMode
+      ? 'linear-gradient(#2c2c2c, #2c2c2c), linear-gradient(#fff, #fff)'
+      : 'linear-gradient(#fff, #fff), linear-gradient(#202020, #202020)'};
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  transition: linear 0.5s;
+  color: ${(props) => (props.isDarkMode ? 'white' : 'black')};
 
-    &:hover {
-        background-image: linear-gradient(#fff, #fff),
-        linear-gradient(to bottom right, #76CAE8, #AD51DE);
-        transition: linear .5s;
-    }
-`;
+  &:hover {
+    background-image: ${(props) =>
+      props.isDarkMode
+        ? 'linear-gradient(#2c2c2c, #2c2c2c), linear-gradient(to bottom right, #76cae8, #ad51de)'
+        : 'linear-gradient(#fff, #fff), linear-gradient(to bottom right, #76cae8, #ad51de)'};
+    transition: linear 0.5s;
+  }
+`
 
 interface ConfirmButtonProps {
-    context: string;
-    onClick?: (event:React.MouseEvent<HTMLButtonElement>) => void;
+  context: string
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const ConfirmButton: React.FC<ConfirmButtonProps> = ({ context, onClick }) => {
-    return (
-        <StyledConfirmButton onClick={onClick}>
-            {context}
-        </StyledConfirmButton>
-    );
+  const isDarkMode = useDarkModeStore((state) => state.isDarkMode)
+  return (
+    <StyledConfirmButton onClick={onClick} isDarkMode={isDarkMode}>
+      {context}
+    </StyledConfirmButton>
+  )
 }
 
-export default ConfirmButton;
+export default ConfirmButton
