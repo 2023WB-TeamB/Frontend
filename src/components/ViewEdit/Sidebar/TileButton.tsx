c93c6dcf230887608f08fa966f8354ee47697328
Haron1248 <natn0367@gmail.com>
2024-01-22 05:44:21 +0900

    design: 사이드바 고정 디자인 추가

--- src/components/ViewEdit/Sidebar/TileButton.tsx
+++ src/components/ViewEdit/Sidebar/TileButton.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { useSidePeekStore } from "../../../store/store";
 
 const Icon = styled.img`
-    width: 32px;
-    height: 32px;
+    width: 28px;
+    height: 28px;
     z-index: 1;
     transition: filter 0.3s;
 `;
 
-const StyledTileButton = styled.button`
+const StyledTileButton = styled.button<{ isOpenSideAlways: boolean }>`
     position: relative;
     display: flex;
     align-items: center;
     justify-content: flex-start;
-    width: 80%;
+    width: ${(props) => 
+        props.isOpenSideAlways ? '120%' : '80%'
+    };
     height: 8vh;
-    min-height: 40px;
+    min-height: 45px;
     background-color: transparent;
     transition: background-image 1s;
     overflow: hidden;
+    transition: all linear .2s;
 
     &:before {
         content: "";
@@ -34,18 +38,26 @@ const StyledTileButton = styled.button`
 
     &:hover {
         border: none;
-        left: 5px;
-        transition: all ease .5s;
+        margin-left: 5px;
+        transition: all linear .2s;
+        border-inline-start: 3px solid black;
+        background-color: #F2F2F2;
     }
 
-    &:focus:before {
+    &:active:before {
         opacity: 1;
     }
 
-    &:focus {
-        ${Icon} {
+    &:active {
+        ${Icon}, label {
             filter: brightness(0) invert(100%) sepia(100%) saturate(0) hue-rotate(0deg);;
         }
+    }
+
+    & label {
+        margin-left: 15px;
+        font-size: small;
+        color: black;
     }
 `;
 
@@ -61,19 +73,21 @@ const DumpArea = styled.div`
 
 interface ButtonProps {
     icon?: string;
+    name?: string;
     onClick?: (event:React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 // 사이드바 타일버튼 기본 양식
-const TileButton: React.FC<ButtonProps> = ({ icon, onClick }) => {
-    const buttonStyle = {
-        height: '8vh',
-    };
+const TileButton: React.FC<ButtonProps> = ({ icon, name, onClick }) => {
+    const {isOpenSideAlways} = useSidePeekStore()
 
     return (
         icon !== "" ? (
-            <StyledTileButton onClick={onClick} style={buttonStyle}>
+            <StyledTileButton onClick={onClick} isOpenSideAlways={isOpenSideAlways}>
                 <Icon src={icon}></Icon>
+                {isOpenSideAlways &&
+                    <label>{name}</label>
+                }
             </StyledTileButton> 
         ) : (
             <DumpArea/>
