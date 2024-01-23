aec113ab23aadd9ad1ae907f300248fbd0db5ec0
오채영 <ocy5007@naver.com>
2024-01-23 14:02:55 +0900

    design: 로그인/회원가입 버튼 길이 수정

--- src/components/GradientBtn.tsx
+++ src/components/GradientBtn.tsx
@@ -4,13 +4,13 @@ import { styled } from 'styled-components'
 
 const GradientBtn = styled.button<{ isDarkMode: boolean }>`
   height: 50px;
-  width: 150px;
+  width: 317px;
   text-align: center;
   text-transform: uppercase;
   transition: 0.5s;
   background-size: 200% auto;
   font-family: 'Inter-Regular', Helvetica;
-  color: white;
+  color: ${(props) => (props.isDarkMode ? 'black' : 'white')};
   background-image: linear-gradient(to right, #79c5e8, #a26be1, #79c5e8);
   box-shadow: 0 0 20px ${(props) => (props.isDarkMode ? '#202020' : '#eee')};
   border-radius: 25px;
