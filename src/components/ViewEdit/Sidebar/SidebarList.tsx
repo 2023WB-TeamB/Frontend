7303c7441e24283be842329144e3d5bc3d68c551
오채영 <ocy5007@naver.com>
2024-01-26 01:02:13 +0900

    design: sidebar border 굵기 및 글씨체 수정

--- src/components/ViewEdit/Sidebar/SidebarList.tsx
+++ src/components/ViewEdit/Sidebar/SidebarList.tsx
@@ -5,10 +5,7 @@ import { useDarkModeStore, useSidePeekStore, useViewerPageOpenStore } from '../.
 
 // 호버 감지 영역
 const WrapperArea = styled.div<{ isOpenSideAlways: boolean }>`
-  position: ${(props) =>
-    props.isOpenSideAlways
-      ? 'relative' : 'fixed'
-  };
+  position: ${(props) => (props.isOpenSideAlways ? 'relative' : 'fixed')};
   height: 80vh;
   width: 85px;
   z-index: 1;
@@ -17,38 +14,26 @@ const WrapperArea = styled.div<{ isOpenSideAlways: boolean }>`
 // 사이드바 스타일
 const Wrapper = styled.div<{ isOpenSide: boolean; isDarkMode: boolean; isOpenSideAlways: boolean }>`
   position: relative;
-  height: ${(props) =>
-    props.isOpenSideAlways
-      ? '100vh' : '80vh'
-  };
-  width: ${(props) =>
-    props.isOpenSideAlways
-      ? '10rem' : '5rem'
-  };
+  height: ${(props) => (props.isOpenSideAlways ? '100vh' : '80vh')};
+  width: ${(props) => (props.isOpenSideAlways ? '10rem' : '5rem')};
   top: 50%;
   transform: translate(${({ isOpenSide }) => (isOpenSide ? `30%` : `-20%`)}, -50%);
   display: flex;
   left: -50px;
   flex-direction: column;
   justify-content: space-between;
-  align-items: ${(props) =>
-    props.isOpenSideAlways
-      ? 'flex-start' : 'flex-end'
-  };
-  border-right: 1px solid transparent;
+  align-items: ${(props) => (props.isOpenSideAlways ? 'flex-start' : 'flex-end')};
+  border-right: 0.8px solid transparent;
   ${(props) =>
     props.isOpenSideAlways
-      ? `` : `border-top: 1px solid transparent;
-      border-bottom: 1px solid transparent;`
-  };
-  border-radius: ${(props) =>
-    props.isOpenSideAlways
-      ? '0' : '10px'
-  };
+      ? ``
+      : `border-top: 0.8px solid transparent;
+      border-bottom: 0.8px solid transparent;`};
+  border-radius: ${(props) => (props.isOpenSideAlways ? '0' : '10px')};
   background-image: ${(props) =>
     props.isDarkMode
-      ? 'linear-gradient(#202020, #202020), linear-gradient(to bottom, #76cae8, #ad51de)'
-      : 'linear-gradient(#fff, #fff), linear-gradient(to bottom, #76cae8, #ad51de)'};
+      ? 'linear-gradient(#202020, #202020), linear-gradient(to bottom, #7CC0E8, #A565E0)'
+      : 'linear-gradient(#fff, #fff), linear-gradient(to bottom, #7CC0E8, #A565E0)'};
   background-origin: border-box;
   background-clip: content-box, border-box;
   transition: linear 0.2s;
@@ -57,11 +42,9 @@ const Wrapper = styled.div<{ isOpenSide: boolean; isDarkMode: boolean; isOpenSid
 
 // list : [icon, name, onClick]
 interface SidebarProps {
-  list: Array<[
-    icon: string, 
-    name?: string,
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
-  ]>
+  list: Array<
+    [icon: string, name?: string, onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void]
+  >
 }
 
 // 사이드바
@@ -71,44 +54,38 @@ const Sidebar: React.FC<SidebarProps> = ({ list }) => {
   const openerStore = useViewerPageOpenStore()
   const isOpenSidePanel = openerStore.isOpenGalleryPanel || openerStore.isOpenVersionPanel
   const [isCursorInArea, setIsCursorInArea] = useState(true)
-  let timeoutId: ReturnType<typeof setTimeout>;
+  let timeoutId: ReturnType<typeof setTimeout>
 
   const handleMouseEnter = () => {
-    setIsCursorInArea(true);
-    clearTimeout(timeoutId); // 이전 타이머를 취소합니다.
-  };
+    setIsCursorInArea(true)
+    clearTimeout(timeoutId) // 이전 타이머를 취소합니다.
+  }
 
   const handleMouseLeave = () => {
     timeoutId = setTimeout(() => {
-      setIsCursorInArea(false);
-    }, 2000);
-  };
+      setIsCursorInArea(false)
+    }, 2000)
+  }
 
   useEffect(() => {
     return () => {
-      clearTimeout(timeoutId); // 컴포넌트가 언마운트될 때 타이머를 취소합니다.
-    };
-  }, []);
+      clearTimeout(timeoutId) // 컴포넌트가 언마운트될 때 타이머를 취소합니다.
+    }
+  }, [])
 
   return (
-    <WrapperArea 
+    <WrapperArea
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      isOpenSideAlways={isOpenSideAlways}
-    >
-      <Wrapper 
-        isOpenSide={isCursorInArea || isOpenSideAlways || isOpenSidePanel} 
-        isDarkMode={isDarkMode} 
-        isOpenSideAlways={isOpenSideAlways}
-      >
+      isOpenSideAlways={isOpenSideAlways}>
+      <Wrapper
+        isOpenSide={isCursorInArea || isOpenSideAlways || isOpenSidePanel}
+        isDarkMode={isDarkMode}
+        isOpenSideAlways={isOpenSideAlways}>
         {list &&
           list.map((item) => {
             const [icon, name, onClick] = item
-            return <TileButton 
-              icon={icon} 
-              name={name} 
-              onClick={onClick}
-            />
+            return <TileButton icon={icon} name={name} onClick={onClick} />
           })}
       </Wrapper>
     </WrapperArea>
