b82ae88f83c525546fc97658e7e3200f2343576a
나희수 <gmltn328@gmail.com>
2024-01-26 21:54:24 +0900

    refactor: 코드 리팩토링(eslint)

--- src/components/useDebounce.tsx
+++ src/components/useDebounce.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-
-const useDebounce = (value: any, delay: number): string => {
+// 일정시간동안 연속적으로 발생되는 이벤트 중에서
+// 마지막만 호출하여 과도한 호출이나 렌더링을 막는 방법
+const useDebounce = (value: string, delay: number): string => {
   const [debounceValue, setDebounceValue] = useState('')
 
   useEffect(() => {
--- src/components/useModalStore.tsx
+++ src/components/useModalStore.tsx
@@ -20,24 +20,25 @@ interface SearchState {
   filteredData: Doc[]
   setFilteredData: (data: Doc[]) => void
 }
-
+//
 export const useModalStore = create<modalState>((set) => ({
+  // 로그인 모달 상태고나리
   isSigninOpen: false,
   toggleSignin: () => set((state) => ({ isSigninOpen: !state.isSigninOpen })),
-
+  // 회원가입 모달 상태관리
   isRegisterOpen: false,
   toggleRegister: () => set((state) => ({ isRegisterOpen: !state.isRegisterOpen })),
-
+  // 검색 모달 상태관리
   isSearchListOpen: false,
   searchListOpen: () => set(() => ({ isSearchListOpen: true })),
   searchListClose: () => set(() => ({ isSearchListOpen: false })),
 }))
-
+// 로컬스토리지 상태관리
 export const useLocalStorageStore = create<localStorageState>((set) => ({
   isGetToken: true,
   setisGetToken: (state) => set({ isGetToken: state }),
 }))
-
+// 검색결과 상태관리
 export const useSearchStore = create<SearchState>((set) => ({
   filteredData: [],
   setFilteredData: (data) => set({ filteredData: data }),
