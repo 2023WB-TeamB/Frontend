ac4f3c1d509f794e5522d5a204b07b6b4249450c
Haron1248 <natn0367@gmail.com>
2024-01-27 04:45:16 +0900

    hotfix: 뷰어 unuse 문제 해결

--- src/components/ViewEdit/Sidebar/VersionPreview.tsx
+++ src/components/ViewEdit/Sidebar/VersionPreview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useDarkModeStore, useDocIdStore } from '../../../store/store'
+import { useDocIdStore } from '../../../store/store'
 import { projectData } from './SidebarPanel';
 import { darken } from 'polished';
 
@@ -8,34 +8,6 @@ interface VersionPreviewProps {
   content: projectData
   isActive?: boolean
 }
-
-// 개별 미리보기 문서 스타일
-const PreviewContent = styled.div<{ isDarkMode: boolean, color: string }>`
-  margin: 6px;
-  min-width: 88px;
-  width: 88px;
-  height: 124.4px;
-  background-color: ${(props) => (props.color)};
-  display: flex;
-  border-radius: 10px;
-  align-items: center;
-  justify-content: center;
-  font-size: 8px;
-  line-height: 1.5;
-  transition: ease 0.3s;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: pre-wrap;
-
-  // 호버시 확장
-  &:hover {
-    margin: 0;
-    min-width: 100px;
-    height: 141.4px;
-    font-size: 9.1px;
-    transition: ease 0.3s;
-  }
-`
 
 const Card = styled.div<{ color: string }>`
   padding: 6px;
