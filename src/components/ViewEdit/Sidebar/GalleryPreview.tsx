7d2b983ef04500ef9ffb4753161952fa4b07b644
Haron1248 <natn0367@gmail.com>
2024-01-27 17:01:02 +0900

    design: 갤러리 디자인 수정

--- src/components/ViewEdit/Sidebar/GalleryPreview.tsx
+++ src/components/ViewEdit/Sidebar/GalleryPreview.tsx
@@ -17,7 +17,7 @@ interface GalleryPreviewProps {
 const PreviewContentWrapper = styled.div<{ isDarkMode: boolean, color: string }>`
   margin-block: 10px;
   width: 100%;
-  height: 8rem;
+  height: 10rem;
   background-color: #f8f8f8;
   border-top-right-radius: 20px;
   border-bottom-right-radius: 20px;
@@ -37,48 +37,65 @@ const TopContent = styled.div`
   justify-content: space-between;
 
   & label {
-    width: 16rem;
-    margin: 0px 5px;
+    width: 20rem;
+    margin: 1rem;
+    margin-right: .2rem;
     font-size: .8rem;
+    font-weight: 700;
     text-align: left;
-    color: #777;
+    color: #888;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   & p {
-    width: 50%;
-    margin: 0px 5px;
-    font-size: .8rem;
+    width: 5rem;
+    margin-right: 1.5rem;
+    font-size: .7rem;
     text-align: right;
   }
 `
 const TitleContent = styled.div`
-  margin-left: 10px;
-  padding: 0px;
-  width: 100%;
-  height: 3rem;
+  margin-left: 1.1rem;
+  padding: .1rem;
+  width: 90%;
+  height: 4rem;
+  line-height: 2rem;
   overflow: hidden;
-  text-overflow: ellipsis;
   text-align: left;
-  font-size: 1.5rem;
+  font-size: 1.35rem;
+  font-weight: 700;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
 `
 const TagContentWrapper = styled.div`
+  position: absolute;
+  bottom: 1.8rem;
   width: 100%;
-  height: 30%;
+  height: 1.5rem;
+  margin: 0 .5rem;
   overflow: hidden;
   display: flex;
   flex-direction: row;
   align-items: center;
-  white-space: nowrap;
+  flex-wrap: wrap;
+  flex-grow: 0;
+  flex-shrink: 0;
 `
-const TagContent = styled.div`
-  padding: 1px 8px;
-  margin-left: 5px;
-  height: 25px;
+const TagContent = styled.div<{ color: string}>`
+  padding: 0 .5rem;
+  margin-left: .5rem;
+  height: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: .7rem;
-  color: #e88;
+  border-radius: 2rem;
+  background: #fff;
+  color: ${(props) => props.color};
 `
 
 // 버튼형 디자인
@@ -147,7 +164,7 @@ const GalleryPreview: React.FC<GalleryPreviewProps> = ({ content }) => {
       </TitleContent>
       <TagContentWrapper>
         {content.keywords.length > 0 && content.keywords.map((item) => {
-          return <TagContent>{item.name}</TagContent>
+          return <TagContent color={content.color}>{item.name}</TagContent>
         })}
       </TagContentWrapper>
     </PreviewContentWrapper>
