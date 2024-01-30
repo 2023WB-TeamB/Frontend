59683d0119f47a43a7cae37b9e39e7a85365cc36
Haron1248 <natn0367@gmail.com>
2024-01-30 18:03:06 +0900

    refactor: 로그아웃 Confirm창 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,14 @@ import Swal from 'sweetalert2'
 import Signin from './Signin'
 import { useModalStore } from './useModalStore'
 import SearchList from './SearchList'
-import { useDarkModeStore } from '../store/store'
+import { useConfirmBoxStore, useDarkModeStore } from '../store/store'
 /*-----------------------------------------------------------*/
 import imgDarkMode from '../assets/images/moon.svg'
 import imgWhiteMode from '../assets/images/sun.svg'
 import imgLogo from '../assets/images/LOGO1.svg'
 import imgSearch from '../assets/images/search.svg'
 import imgSearchDark from '../assets/images/search_dark.svg'
+import ModalConfirm from './ViewEdit/ModalConfirm'
 
 // 인터페이스
 interface HeaderType {
@@ -81,7 +82,16 @@ const SignInOut = styled.div<SignType>`
 const Header: React.FC<HeaderType> = ({ isGetToken }) => {
   const { $isDarkMode, toggleDarkMode } = useDarkModeStore()
   const { isSigninOpen, toggleSignin, isSearchListOpen, searchListOpen } = useModalStore()
+  const { setConfirmAction, openConfirm, setConfirmLabel } = useConfirmBoxStore()
   const navigate = useNavigate()
+
+  // * Toast 알림창
+  const ToastInfor = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 1800,
+  })
 
   // 로그인 모달 클릭 이벤트
   const handleClickSignin = () => {
@@ -97,39 +107,27 @@ const Header: React.FC<HeaderType> = ({ isGetToken }) => {
     searchListOpen()
   }
   // 로그아웃 API 호출 이벤트
-  const handleClickSignout = async () => {
+  const handleSignout = async () => {
     const url = 'https://gitodoc.kro.kr/api/v1/auth' // 배포 서버
     const response = await axios.delete(url)
-    // 로그아웃 알림창
-    Swal.fire({
-      title: 'Sign out',
-      text: 'Do you want to signed out?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'OK',
-      cancelButtonText: 'Cancel',
-    }).then((result) => {
-      // 로그아웃 확인 클릭 시
-      if (result.isConfirmed) {
-        Swal.fire({
-          // title: '로그아웃',
-          text: "You're signed out of GiToDoc",
-          icon: 'success',
-          confirmButtonColor: '#3085d6',
-          confirmButtonText: 'OK',
-        })
-        // 로그아웃 성공 시
-        if (response.status === 202) {
-          console.log('API Response: ', response.status)
-          // 로컬스토리지에서 토큰 삭제
-          localStorage.removeItem('accessToken')
-          localStorage.removeItem('refreshToken')
-          navigate('/') // 메인페이지로 이동
-        }
-      }
-    })
+    // 로그아웃 성공 시
+    if (response.status === 202) {
+      ToastInfor.fire({
+        icon: 'success',
+        title: "You're signed out of GiToDoc",
+      })
+      console.log('API Response: ', response.status)
+      // 로컬스토리지에서 토큰 삭제
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      navigate('/') // 메인페이지로 이동
+    }
+  }
+
+  const handleConfirmSignout = () => {
+    setConfirmLabel("Do you want to signed out?")
+    setConfirmAction(handleSignout)
+    openConfirm()
   }
 
   return (
@@ -173,7 +171,7 @@ const Header: React.FC<HeaderType> = ({ isGetToken }) => {
               onClick={handleDarkMode}
             />
             {/* 로그아웃 */}
-            <SignInOut $isDarkMode={$isDarkMode} onClick={handleClickSignout}>
+            <SignInOut $isDarkMode={$isDarkMode} onClick={handleConfirmSignout}>
               Sign-out
             </SignInOut>
           </RightWrapper>
@@ -182,6 +180,7 @@ const Header: React.FC<HeaderType> = ({ isGetToken }) => {
       {/* 모달 */}
       {isSearchListOpen && <SearchList />}
       {isSigninOpen && <Signin />}
+      <ModalConfirm />
     </>
   )
 }
